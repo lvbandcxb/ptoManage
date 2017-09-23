819e422c39a69a88e715176eb93116355762b32b
cxbandlvb <handsomecxb@163.com>
2017-09-23 19:04:04 +0800

    no message

--- api.php
+++ api.php
@@ -7,12 +7,13 @@ if( empty($_POST['status']) ){
     exit;
 }else if( $_POST['status'] == "checkLogin" ){
     $user_name = $_POST['user_name'];
-    $user_password = md5( md5('123456').$key );
+    $user_password = md5( md5( $_POST['user_password'] ).$key );
     $list = $db->table('user')->where("user_name='$user_name' and user_password='$user_password'")->select();
     $list = count($list);
     $url='http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"]; 
     if($list == 1){
         echo "<script>location.replace(".$url."/upfile/uploadfile.html".")</script>";
+        echo "1";
     }else{
         echo "<script>location.replace(".$url."login.html?error".")</script>";
     }
@@ -22,6 +23,26 @@ if( empty($_POST['status']) ){
 	$arr['pto_url'] = $_POST['pto_url'];
 	$arr['pto_create_time'] = time();
 	$res = $db->table('photo')->insert($arr);
+	if($res){
+		echo "增加成功！";
+	}else{
+		echo "增加失败！";
+	}
+}else if( $_POST['status'] == "addAlbum" ){
+	$arr['alb_name'] = $_POST['alb_name'];
+	$arr['alb_pid'] = $_POST['alb_pid'];
+	$arr['alb_create_time'] = time();
+	$res = $db->table('album')->insert($arr);
+	if($res){
+		echo "增加成功！";
+	}else{
+		echo "增加失败！";
+	}
+}else if ( $_POST['status'] == "addUser" ){
+	$arr['user_name'] = $_POST['user_name'];
+	$arr['user_password'] = md5( md5( $_POST['user_password'] ).$key );
+	$arr['user_power'] = $_POST['user_power'];
+	$res = $db->table("user")->insert($arr);
 	if($res){
 		echo "增加成功！";
 	}else{
