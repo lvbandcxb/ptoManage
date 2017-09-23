70e92c3eecb9067a2e5d3b04e8334cb9fbfd5fec
cxbandlvb <handsomecxb@163.com>
2017-09-23 18:20:07 +0800

    更新登录接口

--- api.php
+++ api.php
@@ -7,13 +7,14 @@ if( empty($_POST['status']) ){
     exit;
 }else if( $_POST['status'] == "checkLogin" ){
     $user_name = $_POST['user_name'];
-    $user_password = md5( md5('123456').'photo' );
+    $user_password = md5( md5('123456').$key );
     $list = $db->table('user')->where("user_name='$user_name' and user_password='$user_password'")->select();
     $list = count($list);
+    $url='http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"]; 
     if($list == 1){
-        echo "登录成功";
+        echo "<script>location.replace(".$url."/upfile/uploadfile.html".")</script>";
     }else{
-        echo "登录失败";
+        echo "<script>location.replace(".$url."login.html?error".")</script>";
     }
 }else if( $_POST['status'] == "insertPhoto" ){
 	$arr['pto_name'] = $_POST['pto_name'];
